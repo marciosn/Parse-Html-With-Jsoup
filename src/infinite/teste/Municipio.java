8c7652e734a6449b8ad46d49be3803b91d9f9eba
marciosn <marciosouzanobre@gmail.com>
2016-05-03 22:42:54 -0300

    certoooooo

---
+++ src/infinite/teste/Municipio.java
@@ -0,0 +1,32 @@
+package infinite.teste;
+
+public class Municipio {
+	
+	private int id;
+	private String codigo;
+	private String nome;
+	
+	public Municipio(){}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
