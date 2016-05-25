e818bacd218e505accd87a1e4571831789fb22cf
marciosn <marciosouzanobre@gmail.com>
2016-05-24 23:15:14 -0300

    funciona caralho

--- src/bolsa/Bolsa.java
+++ src/bolsa/Bolsa.java
@@ -1,105 +1,33 @@
 package bolsa;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import com.google.gson.Gson;
 
 public class Bolsa {
 	
 	public static void main(String[] args) {
-
-		List<CearaCidades> cidades = new ArrayList<>();
-		String url = "http://bolsa-familia.com/cidades/ceara/8/1/1";
-		Document doc;
-		try {
-			doc = Jsoup.connect(url).get();		
-			Element table = doc.select("table[class=table]").first();
-			Elements links = null;
-			//List<Element> tds = table.select("td");
-			List<Element> trs = table.select("tr");
-			
-			int tamanho = 84;
-			
-			for (int k = 1; k <= tamanho; k++) {
-				String urlNew = "http://bolsa-familia.com/pessoas/ceara/abaiara/"+ k +"/1/1";
-				System.out.println("### "+urlNew);
-				doc = Jsoup.connect(urlNew).timeout(5000).get();
-				Element t = doc.select("table[class=table]").first();
-				List<Element> trList = t.select("tr");
-				//System.out.println(t);
-				
-				
-				for(int i = 0; i < trList.size(); i++){
-					CearaCidades cearaCidades;
-					List<Element> rows = trList.get(i).select("td");
-					if (trList.get(i).text().contains("Ver Pessoas") || trList.get(i).text().contains("Ver Pagamentos")) {
-						continue;
-					} else {
-						if ( rows.size() == 3) {
-							cearaCidades = new CearaCidades();
-							cearaCidades.setNome(rows.get(0).text());
-							cearaCidades.setPagamentos(rows.get(1).text());
-							cearaCidades.setValor(rows.get(2).text()); 
-							cearaCidades.setUrlCidade(rows.get(0).select("a[href]").attr("href"));
-							cidades.add(cearaCidades);
-						}
-					}
-				}
-			}
-			
-			Gson gson = new Gson();
-			System.out.println(gson.toJson(cidades));
-			ObjectMapper mapper = new ObjectMapper();  
-			System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(cidades));
-			
-			/*for(int i = 0; i < trs.size(); i++){
-				CearaCidades cearaCidades;
-				List<Element> rows = trs.get(i).select("td");
-				if (trs.get(i).text().contains("Ver Pessoas") || trs.get(i).text().contains("Ver Pagamentos")) {
-					continue;
-				} else {
-					if ( rows.size() == 3) {
-						cearaCidades = new CearaCidades();
-						System.out.println(rows.get(0).text());
-						System.out.println(rows.get(1).text());
-						System.out.println(rows.get(2).text());
-						System.out.println(rows.get(0).select("a[href]").attr("href"));
-						
-						cearaCidades.setNome(rows.get(0).text());
-						cearaCidades.setPagamentos(rows.get(1).text());
-						cearaCidades.setValor(rows.get(2).text()); 
-						cearaCidades.setUrlCidade(rows.get(0).select("a[href]").attr("href"));
-						cidades.add(cearaCidades);
-					}
-				}
-			}
-			
-			//Gson gson = new Gson();
-			//System.out.println(gson.toJson(cidades));
-			ObjectMapper mapper = new ObjectMapper();  
-			System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(cidades));
-*/
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
+		getPessoasCidade("ceara", "quixada");
 	}
 	
-	public static void getPessoasCidade(String estado){
+	public static void getPessoasCidade(String estado ,String cidade){
 		List<CearaCidades> cidades = new ArrayList<>();
 		Document doc;
 		try {			
 			int tamanho = 84;
 			
 			for (int k = 1; k <= tamanho; k++) {
-				String urlNew = "http://bolsa-familia.com/pessoas/"+estado.toLowerCase()+"/abaiara/"+ k +"/1/1";
+				String urlNew = "http://bolsa-familia.com/pessoas/"+estado.toLowerCase()+"/"+ cidade +"/"+ k +"/1/1";
 				System.out.println("### "+urlNew);
 				doc = Jsoup.connect(urlNew).timeout(5000).get();
 				Element t = doc.select("table[class=table]").first();
@@ -127,10 +55,34 @@ public class Bolsa {
 			System.out.println(gson.toJson(cidades));
 			ObjectMapper mapper = new ObjectMapper();  
 			System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(cidades));
+			
+			createJSON(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(cidades),"ceara","abaiara");
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void createJSON(String json, String estado, String cidade){
+		
+		 	File file = new File(estado + "_" + cidade+ ".json");
+		    FileWriter writer = null;
+		    try {
+		        writer = new FileWriter(file);
+		        writer.write(json);
+		    } catch (IOException e) {
+		        e.printStackTrace(); // I'd rather declare method with throws IOException and omit this catch.
+		    } finally {
+		        if (writer != null) try { writer.close(); } catch (IOException ignore) {}
+		    }
+		    System.out.printf("File is located at %s%n", file.getAbsolutePath());
+		
+/*		try (FileWriter file = new FileWriter("/Jsoup/json/" + estado + "_" + cidade + ".json")) {
+			file.write(json);
+			System.out.println("Successfully Copied JSON Object to File...");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
 	}
 
 }
